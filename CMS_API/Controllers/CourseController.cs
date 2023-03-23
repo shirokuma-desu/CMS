44eba527fa7ccb92f6918829edfb470f1d6984a0
shirokuma-desu <ezorcist.dt@gmail.com>
2023-03-23 14:03:51 +0700

    Add new controller

--- CMS_API/Controllers/CourseController.cs
+++ CMS_API/Controllers/CourseController.cs
@@ -1,5 +1,6 @@
 ﻿using CMS_API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -21,19 +22,27 @@ namespace CMS_API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAll()
         {
-            var context = await _context.Courses.Include(lm => lm.LearningMaterials).Include(a=>a.Assignments).ToListAsync();
+            var context = await _context.Courses.Include(lm => lm.LearningMaterials).Include(a=>a.Assignments).Include( u => u.Teacher).ToListAsync();
             return Ok(context);
         }
 
         // GET api/<Course>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public  async Task<IActionResult> GetCourseId(int id)
         {
-            return "value";
+            try
+            {
+                var context = await _context.Courses.FirstOrDefaultAsync(c => c.Id == id);
+                return Ok(context);
+
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         // POST api/<Course>
-        [HttpPost]
+        [HttpPost("CreateCourse/{teacher_id}")]
         public async Task<IActionResult> Post(string coursename,string code,int teacher_id)
         {
             Course c = new Course
@@ -55,8 +64,20 @@ namespace CMS_API.Controllers
 
         // DELETE api/<Course>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            try
+            {
+                var context = await _context.Courses.FirstOrDefaultAsync(c => c.Id == id);
+                
+                    var c = _context.Courses.Remove(context);
+                     await _context.SaveChangesAsync();
+                    return Ok();
+            }catch
+            (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
     }
 }
