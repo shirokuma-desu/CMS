f0d3e25696dd6055862f2e327ede474941468412
= <=>
2023-03-26 02:29:57 +0700

    add login

--- Client/Controllers/HomeController.cs
+++ Client/Controllers/HomeController.cs
@@ -1,16 +1,20 @@
 ﻿using Client.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
 
 namespace Client.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private readonly HttpClient _httpClient;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://your-api.com/");
         }
 
         public IActionResult Index()
@@ -22,10 +26,24 @@ namespace Client.Controllers
         {
             return View();
         }
+
         [HttpPost]
-        public IActionResult Login(string username, string password)
+        public async Task<IActionResult> Login(string username, string password)
         {
-            return View();
+            var loginModel = new { Username = username, Password = password };
+            var response = await _httpClient.PostAsJsonAsync("https://localhost:7087/api/User/", loginModel);
+            if (response.IsSuccessStatusCode)
+            {
+                var jwtToken = await response.Content.ReadAsStringAsync();
+                HttpContext.Session.SetString("Token", jwtToken);
+                HttpContext.Session.SetString("username", username);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Username or password is wrong";
+                return View();
+            }
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
